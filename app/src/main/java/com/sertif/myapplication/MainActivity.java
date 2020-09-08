81aa1435f0a94585febfe26e2d826458087fb27a
Bella Setyowati <bella.setyowati99@gmail.com>
2020-09-08 13:01:08 +0700

    Penambahan log di MainActivity.java

--- app/src/main/java/com/sertif/myapplication/MainActivity.java
+++ app/src/main/java/com/sertif/myapplication/MainActivity.java
@@ -3,12 +3,19 @@ package com.sertif.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MyActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.e(TAG, ", Error");
+        Log.i(TAG, ", Informasi ");
+        Log.w(TAG, ", Warning");
+        Log.d(TAG, ", Debug");
+        Log.v(TAG, ", Verbose");
     }
 }
